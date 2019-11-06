deac128a20fa21c9c8aa6d66be1339fca840debf
Jeyaprakash <Jeya.George@scouting.org>
2019-11-06 22:24:11 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const oxcowbull = (name, suggession) => {
+    var suggArr= suggession.split("");
+
+    var oxcount=0;
+    var bullcount=0;
+    var cowcount=0;
+    suggArr.forEach((element,i) => {
+        console.log(element,i);
+        if(name.indexOf(element)>-1){
+            if(name.indexOf(element)==i){
+                cowcount=cowcount+1;
+            }else{
+                bullcount=bullcount+1;
+            }
+
+        }else{
+            oxcount=oxcount+1;
+        }
+    });
+
+    var result = "";
+    if(oxcount>0){
+        result= result+oxcount+" Ox";
+    }
+    if(cowcount>0){
+        result= result+" "+cowcount+" Cow";
+    }
+    if(bullcount>0){
+        result= result+" "+bullcount+" Bull";
+    }
+
+    return result;
+}
+module.exports = oxcowbull;
